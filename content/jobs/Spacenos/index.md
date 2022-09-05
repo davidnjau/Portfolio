5af66f6435c7f682ab6841ede916f9b76a1a15b2
Dave <davidnjau21@gmail.com>
2022-09-05 09:12:51 +0300

    first commit

---
+++ content/jobs/Spacenos/index.md
@@ -0,0 +1,14 @@
+---
+date: '2021-09-13'
+title: 'Android Developer Intern'
+company: 'Spacenos'
+location: 'Remote, IN'
+range: 'September 2021 - December 2021'
+url: 'https://spacenos.com/'
+---
+
+<ul>
+    <li>Modernized real-time communication by integrating it with Quickblox API and enabled payments through Stripe.</li>
+    <li>Designed and Developed application logic for couple of mobile apps using Android SDK and Java for couple of international clients.</li>
+    <li>Involved in the Development of various CRUD functionality on Firebase Firestore and RealtimeDB.</li>
+</ul>
---
+++ src/components/sections/about.js
@@ -0,0 +1,185 @@
+import React, { useEffect, useRef } from 'react';
+import { StaticImage } from 'gatsby-plugin-image';
+import styled from 'styled-components';
+import { srConfig } from '@config';
+import sr from '@utils/sr';
+import { usePrefersReducedMotion } from '@hooks';
+
+const StyledAboutSection = styled.section`
+  max-width: 900px;
+
+  .inner {
+    display: grid;
+    grid-template-columns: 3fr 2fr;
+    grid-gap: 50px;
+
+    @media (max-width: 768px) {
+      display: block;
+    }
+  }
+`;
+const StyledText = styled.div`
+  ul.skills-list {
+    display: grid;
+    grid-template-columns: repeat(2, minmax(140px, 200px));
+    padding: 0;
+    margin: 20px 0 0 0;
+    overflow: hidden;
+    list-style: none;
+
+    li {
+      position: relative;
+      margin-bottom: 10px;
+      padding-left: 20px;
+      font-family: var(--font-mono);
+      font-size: var(--fz-xs);
+
+      &:before {
+        content: '▹';
+        position: absolute;
+        left: 0;
+        color: var(--green);
+        font-size: var(--fz-sm);
+        line-height: 12px;
+      }
+    }
+  }
+`;
+const StyledPic = styled.div`
+  position: relative;
+  max-width: 300px;
+
+  @media (max-width: 768px) {
+    margin: 50px auto 0;
+    width: 70%;
+  }
+
+  .wrapper {
+    ${({ theme }) => theme.mixins.boxShadow};
+    display: block;
+    position: relative;
+    width: 100%;
+    border-radius: var(--border-radius);
+    background-color: var(--green);
+
+    &:hover,
+    &:focus {
+      background: transparent;
+      outline: 0;
+
+      &:after {
+        top: 15px;
+        left: 15px;
+      }
+
+      .img {
+        filter: none;
+        mix-blend-mode: normal;
+      }
+    }
+
+    .img {
+      position: relative;
+      border-radius: var(--border-radius);
+      mix-blend-mode: multiply;
+      filter: grayscale(100%) contrast(1);
+      transition: var(--transition);
+    }
+
+    &:before,
+    &:after {
+      content: '';
+      display: block;
+      position: absolute;
+      width: 100%;
+      height: 100%;
+      border-radius: var(--border-radius);
+      transition: var(--transition);
+    }
+
+    &:before {
+      top: 0;
+      left: 0;
+      background-color: var(--navy);
+      mix-blend-mode: screen;
+    }
+
+    &:after {
+      border: 2px solid var(--green);
+      top: 20px;
+      left: 20px;
+      z-index: -1;
+    }
+  }
+`;
+
+const About = () => {
+  const revealContainer = useRef(null);
+  const prefersReducedMotion = usePrefersReducedMotion();
+
+  useEffect(() => {
+    if (prefersReducedMotion) {
+      return;
+    }
+
+    sr.reveal(revealContainer.current, srConfig());
+  }, []);
+
+  const skills = [
+    'Java',
+    'Kotlin',
+    'Android Development',
+    'Firebase',
+    'MySQL',
+    'Golang',
+    'HTML & CSS',
+    'JavaScript',
+    'Node.js',
+    'C++'
+  ];
+
+  return (
+    <StyledAboutSection id="about" ref={revealContainer}>
+      <h2 className="numbered-heading">About Me</h2>
+
+      <div className="inner">
+        <StyledText>
+          <div>
+            <p>
+              Hello, I am a Software Engineer with 1+ years of experience who loves solving real-world complex problems.
+              Worked on different technologies for developing various solutions on the web, command line, and android to bring the best digital experiences to the user.
+            </p>
+            <p>
+              I enjoy working on projects in a team full of excited people who wants to learn and
+              grow like me and In order to keep learning new things, I am always looking for projects and opportunities that will help me learn and upskill better.
+            </p>
+            <p>
+              Also, I document my developer journey and learnings on Medium (with 10,000+ monthly
+              readers) to help new developers in their journey.
+            </p>
+            <p>Here are a few technologies I've worked and been working with recently:</p>
+          </div>
+
+          <ul className="skills-list">
+            {skills && skills.map((skill, i) => <li key={i}>{skill}</li>)}
+          </ul>
+        </StyledText>
+
+        <StyledPic>
+          <div className="wrapper">
+            <StaticImage
+              className="img"
+              src="../../images/me.jpg"
+              width={1024}
+              quality={100}
+              formats={['AUTO', 'WEBP', 'AVIF']}
+              alt="Headshot"
+            />
+          </div>
+        </StyledPic>
+      </div>
+    </StyledAboutSection>
+  );
+};
+
+export default About;
